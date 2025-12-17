49e0ffb3b36c4f3989b0dbb704f63065c3d8793a
Marcelo <26576174+marcelo9987@users.noreply.github.com>
2025-12-18 00:35:56 +0100

    Initial commit

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,39 @@
+import {IResolvers} from "@graphql-tools/utils";
+import {createUser, validateUser} from "../collections/users";
+import {signToken} from "../auth";
+
+export const resolvers: IResolvers =
+    {
+        Query: {
+            me:async(_,__,{user}) =>
+            {
+                if(!user)
+                {
+                    throw new Error('No autenticado');
+                }
+                return {
+                    _id: user._id.toString(),
+                    ...user
+                }
+            }
+        },
+
+
+        Mutation: {
+            register: async (_, {email, password}) =>
+            {
+                const idUsuarioCreado = await createUser(email, password);
+                return signToken(idUsuarioCreado);
+            },
+            login: async (_, {email, password}) =>
+            {
+                const usuario = await validateUser(email, password);
+                if (!usuario)
+                {
+                    throw new Error('Usuario no encontrado o contraseña incorrecta');
+                }
+
+                return signToken(usuario._id.toString());
+            },
+        }
+    };
---
+++ src/utils.ts
@@ -0,0 +1,2 @@
+export const dbName = 'final';
+export const USER_COLLECTION = 'users';
